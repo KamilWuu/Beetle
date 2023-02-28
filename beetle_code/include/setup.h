d98a8b5845fb12315bd1b2f74a5fd8a0d610b3e2
TheSimper <kamilptakiprzygody@gmail.com>
2023-02-28 15:07:31 +0100

    Utworzono kod i definicje pinow

---
+++ beetle_code/include/setup.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <Arduino.h>
+
+//----LEDS----//
+#define LED_RED 13
+#define LED_BLUE 12
+
+//----MOTORS----//
+#define MOTOR_LEFT_1 10
+#define MOTOR_LEFT_2 9
+
+#define MOTOR_RIGHT_1 6
+#define MOTOR_RIGHT_2 5
+
+//----SERVOS----//
+#define SERVO_LEFT 11
+#define SERVO_RIGHT 3
+
+//----TOF_SENSORS_PINS----//
+#define TOF_PIN_1 8
+#define TOF_PIN_2 7
+#define TOF_PIN_3 4
+#define TOF_PIN_4 2
+
+//----GROUND_SENSORS----//
+#define GROUND_SENSOR_LEFT A7
+#define GROUND_SENSOR_RIGHT A6
+
+//----SWITCHES----//
+#define SWITCH_2 A0
+#define SWITCH_3 A1
+
+//----VOLTAGE_MEASUREMENT----//
+#define VOLTAGE A2
+
+//----STARTER----//
+#define STARTER A3
---
+++ beetle_code/src/main.cpp
@@ -0,0 +1,60 @@
+#include "setup.h"
+
+
+int main(void)
+{
+  /////////Arduino stuff//////////
+  init();
+#if defined(USBCON)
+  USBDevice.attach();
+#endif
+  ////////////////////////////////
+    //setup() here:
+	
+//pinModes:
+//----LEDS----//
+pinMode(LED_RED, OUTPUT);
+pinMode(LED_BLUE, OUTPUT);
+
+//----MOTORS----//
+pinMode(MOTOR_LEFT_1, OUTPUT);
+pinMode(MOTOR_LEFT_2, OUTPUT);
+pinMode(MOTOR_RIGHT_1, OUTPUT);
+pinMode(MOTOR_RIGHT_2, OUTPUT);
+
+//----SERVOS----//
+pinMode(SERVO_LEFT, OUTPUT);
+pinMode(SERVO_RIGHT, OUTPUT);
+
+//----TOF_SENSORS_PINS----//
+pinMode(TOF_PIN_1, OUTPUT);
+pinMode(TOF_PIN_2, OUTPUT);
+pinMode(TOF_PIN_3, OUTPUT);
+pinMode(TOF_PIN_4, OUTPUT);
+
+//----GROUND_SENSORS----//
+pinMode(GROUND_SENSOR_LEFT, INPUT);
+pinMode(GROUND_SENSOR_RIGHT, INPUT);
+
+//----SWITCHES----//
+pinMode(SWITCH_2, INPUT);
+pinMode(SWITCH_3, INPUT);
+
+//----VOLTAGE_MEASUREMENT----//
+pinMode(VOLTAGE, INPUT);
+
+//----STARTER----//
+pinMode(STARTER, INPUT);
+
+
+  for (;;)
+  { // void loop() equivalent
+    
+
+    ////////////Arduino stuff/////////////
+    if (serialEventRun)
+      serialEventRun();
+    //////////////////////////////////////
+  }
+  return 0;
+}
